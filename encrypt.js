621c19f4bfa01ad178fe9f8fed272bb879303579
davidionita <31045833+davidionita@users.noreply.github.com>
2018-06-06 16:11:03 -0400

    Completed encryption and decryption.

--- encrypt.js
+++ encrypt.js
@@ -1,20 +1,38 @@
-function encrypt () {
-    var fs = require('fs');
-    var crypto = require('crypto');
+var fs = require('fs');
+var crypto = require('crypto');
+var key = crypto.randomBytes(6).toString('base64');
+var fileName = crypto.randomBytes(9).toString('base64');
+console.log(fileName);
 
-    var key = '14189dc35ae35e75ff31d7502e245cd9bc7803838fbfd5c773cdcd79b8a28bbd';
+
+console.log(key);
+
+function encrypt () {
     var cipher = crypto.createCipher('aes-256-cbc', key);
     var input = fs.createReadStream('test.txt');
-    var output = fs.createWriteStream('kfjdkFFE552.hahaha');
+    var output = fs.createWriteStream(fileName);
 
     input.pipe(cipher).pipe(output);
-
 
     output.on('finish', function() {
         console.log('Encrypted file written to disk!');
     });
 
-
 }
 
 encrypt();
+
+function decrypt () {
+    var decipher = crypto.createDecipher('aes-256-cbc', key);
+    var input = fs.createReadStream(fileName);
+    var output = fs.createWriteStream('done.txt');
+
+    input.pipe(decipher).pipe(output);
+
+    output.on('finish', function() {
+        console.log('Decrypted file written to disk!');
+    });
+
+}
+
+setTimeout(decrypt, 2500);
