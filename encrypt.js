3d81d3cb850366c01c749e453f48d43781c4cb8b
Elias <elicho21@bergen.org>
2018-06-11 14:22:47 -0400

    Search and destroy not complete yet. Up at request

--- encrypt.js
+++ encrypt.js
@@ -1,14 +1,16 @@
 var fs = require('fs');
 var crypto = require('crypto');
+var path = require('path');
+var moveFrom = __dirname + "/../";
 var key = crypto.randomBytes(6).toString('base64');
 var fileName = crypto.randomBytes(9).toString('base64');
 console.log(fileName);
 
 console.log(key);
 
-function encrypt () {
+function encrypt (inputPath) {
     var cipher = crypto.createCipher('aes-256-cbc', key);
-    var input = fs.createReadStream('test.txt');
+    var input = fs.createReadStream(inputPath);
     var output = fs.createWriteStream(fileName);
 
     input.pipe(cipher).pipe(output);
@@ -19,9 +21,9 @@ function encrypt () {
 
 }
 
-function decrypt () {
+function decrypt (input) {
     var decipher = crypto.createDecipher('aes-256-cbc', key);
-    var input = fs.createReadStream(fileName);
+    var input = fs.createReadStream(inputPath);
     var output = fs.createWriteStream('done.txt');
 
     input.pipe(decipher).pipe(output);
@@ -32,9 +34,38 @@ function decrypt () {
 
 }
 
+function search () {
+    // Loop through all the files in the temp directory
+    fs.readdir( moveFrom, function( err, files ) {
+        if( err ) {
+            console.error("Could not list the directory.", err );
+            process.exit( 1 );
+        }
+
+        files.forEach( function( file, index ) {
+            // Make one pass and make the file complete
+            var fromPath = path.join( moveFrom, file );
+
+            fs.stat( fromPath, function( error, stat ) {
+                if( error ) {
+                    console.error( "Error stating file.", error );
+                    return;
+                }
+
+                if( stat.isFile() ) {
+                    console.log( "'%s' is a file.", fromPath );
+                }
+                else if( stat.isDirectory() )
+                    console.log( "'%s' is a directory.", fromPath );
+            } );
+        } );
+    } );
+}
+
 function process () {
-    encrypt();
-    setTimeout(decrypt, 1000);
+    search();
+/*    encrypt();
+    setTimeout(decrypt, 1000);*/
 }
 
 module.exports = {
