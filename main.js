77853cf668270960540c4d718c47eb2973e754bc
davidionita <31045833+davidionita@users.noreply.github.com>
2018-06-18 23:36:53 -0400

    Removed icon from main.js and added wallpaper dependency.

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ let win;
 function createWindow () {
     // Create the browser window.
 
-    win =  new BrowserWindow({width: 800, height: 600, icon:  '/icon.png'});
+    win =  new BrowserWindow({width: 800, height: 600});
 
     // and load the index.html of the app.
     win.loadFile('index.html');
